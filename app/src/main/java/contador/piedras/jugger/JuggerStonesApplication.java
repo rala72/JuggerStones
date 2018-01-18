787e7215d1127f7340e5cb0bc1c5c78fcf84ed1c
rala72 <code@rala.io>
2018-01-18 14:57:35 +0100

    added updateSound || reorganized Application class

--- app/src/main/java/contador/piedras/jugger/JuggerStonesApplication.java
+++ app/src/main/java/contador/piedras/jugger/JuggerStonesApplication.java
@@ -12,11 +12,7 @@ import android.preference.PreferenceManager;
 
 import contador.piedras.jugger.model.Sound;
 
-/**
- * Created by rala on 17.01.2018.
- */
-
-public class JuggerStonesApplication extends Application {
+public class JuggerStonesApplication extends Application implements SharedPreferences.OnSharedPreferenceChangeListener {
 
     public static SharedPreferences sharedPreferences;
     public static AudioManager audioManager;
@@ -45,9 +41,22 @@ public class JuggerStonesApplication extends Application {
         super.onCreate();
         PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
         sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        sharedPreferences.registerOnSharedPreferenceChangeListener(this);
         audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
-        String soundStone = JuggerStonesApplication.sharedPreferences.getString(JuggerStonesApplication.PREFS.SOUND_STONE.toString(), "stone");
-        String soundGong = JuggerStonesApplication.sharedPreferences.getString(JuggerStonesApplication.PREFS.SOUND_GONG.toString(), "gong");
+        updateSound();
+    }
+
+    //region sound & volume
+    public static void updateSound() {
+        updateSound(null, null);
+    }
+
+    @SuppressWarnings("SameParameterValue")
+    public static void updateSound(String soundStone, String soundGong) {
+        if (soundStone == null)
+            soundStone = JuggerStonesApplication.sharedPreferences.getString(JuggerStonesApplication.PREFS.SOUND_STONE.toString(), "stone");
+        if (soundGong == null)
+            soundGong = JuggerStonesApplication.sharedPreferences.getString(JuggerStonesApplication.PREFS.SOUND_GONG.toString(), "gong");
         sound = new Sound(soundStone, soundGong);
     }
 
@@ -58,18 +67,9 @@ public class JuggerStonesApplication extends Application {
     public static void decreaseVolume() {
         audioManager.adjustStreamVolume(AudioManager.STREAM_MUSIC, AudioManager.ADJUST_LOWER, AudioManager.FLAG_SHOW_UI);
     }
+    //endregion
 
-    public static void sendEmail(Context context) {
-        Intent intent = new Intent(Intent.ACTION_SENDTO);
-        //noinspection SpellCheckingInspection
-        intent.setData(Uri.parse("mailto:"));
-        intent.putExtra(Intent.EXTRA_EMAIL, new String[]{context.getString(R.string.email_current)});
-        intent.putExtra(Intent.EXTRA_SUBJECT, context.getString(R.string.app_name) + JuggerStonesApplication.getVersionForEmail(context));
-        intent.putExtra(Intent.EXTRA_TEXT, "");
-        context.startActivity(intent);
-    }
-
-    //region version
+    //region version & email
     public static String getVersion(Context context) {
         try {
             PackageInfo pInfo = context.getPackageManager().getPackageInfo(context.getPackageName(), 0);
@@ -97,5 +97,21 @@ public class JuggerStonesApplication extends Application {
 
         return ":" + version_txt + ":" + version_code;
     }
+
+    public static void sendEmail(Context context) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        //noinspection SpellCheckingInspection
+        intent.setData(Uri.parse("mailto:"));
+        intent.putExtra(Intent.EXTRA_EMAIL, new String[]{context.getString(R.string.email_current)});
+        intent.putExtra(Intent.EXTRA_SUBJECT, context.getString(R.string.app_name) + JuggerStonesApplication.getVersionForEmail(context));
+        intent.putExtra(Intent.EXTRA_TEXT, "");
+        context.startActivity(intent);
+    }
     //endregion
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        if (key.equals(PREFS.SOUND_STONE.toString()) || key.equals(PREFS.SOUND_GONG.toString()))
+            updateSound();
+    }
 }
