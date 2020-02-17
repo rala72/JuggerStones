268f27e72981445d13ba5468730978a379a9aea8
rala72 <code@rala.io>
2020-02-17 20:18:31 +0100

    shorten reverted line

--- app/src/main/java/io/rala/jugger/JuggerStonesApp.java
+++ app/src/main/java/io/rala/jugger/JuggerStonesApp.java
@@ -263,7 +263,8 @@ public class JuggerStonesApp extends Application
             if (interval == 0)
                 interval = new BigDecimal(
                     sharedPreferences.getString(
-                        PREFS.INTERVAL_CUSTOM.toString(), String.valueOf(DEFAULT_INTERVAL)
+                        PREFS.INTERVAL_CUSTOM.toString(),
+                        String.valueOf(DEFAULT_INTERVAL)
                     )
                 ).multiply(BigDecimal.valueOf(1000)).longValue();
             return interval <= 0 ? 1 : interval; // just to make sure
